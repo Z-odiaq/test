d8a5255184279386655c158bbf8a438f3875c8aa
Mohamed Ammar <mohamedammar78@yahoo.com>
2024-03-05 00:23:14 +0100

    10

--- src/main/java/com/example/test/fromExcel.java
+++ src/main/java/com/example/test/fromExcel.java
@@ -22,24 +22,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class fromExcel {
-
+    static String datesField = "";
+    static String datesFile = "";
+    static String dateFormat = "";
     public static void main(String[] args) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.enable(JsonParser.Feature.ALLOW_COMMENTS);
-            JsonNode regexRoot = objectMapper.readTree(new File("C:/Users/moham/IdeaProjects/test/src/main/java/com/example/test/regex.json"));
-            List<String> dateExamples = readLatestStartDatesFromExcel("C:/Users/moham/IdeaProjects/test/src/main/java/com/example/test/dates.xlsx");
+            JsonNode regexRoot = objectMapper.readTree(new File("C:/Users/moham/IdeaProjects/test/src/main/java/com/example/test/config.json"));
             Map<String, String> datePatternsMap = readRegexFromJson(regexRoot);
+            List<String> dateExamples = readLatestStartDatesFromExcel(datesFile);
+
 
 
             Map<String, String> results = detectDateFormatWithHashMap(dateExamples, datePatternsMap);
 
-            for (Map.Entry<String, String> entry : results.entrySet()) {
-                System.out.println("With HashMap | Input: " + entry.getKey() + " | Detected Format: " + entry.getValue());
-            }
+           // for (Map.Entry<String, String> entry : results.entrySet()) {
+            //    System.out.println("Input: " + entry.getKey() + " | Detected Format: " + entry.getValue());
+            //}
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error reading JSON file: " + e.getMessage());
+        }
+    }
+
+    private static boolean isJSONValid(String test) {
+        try {
+            new ObjectMapper().readTree(test);
+            return true;
+        } catch (IOException e) {
+            return false;
         }
     }
 
@@ -55,7 +67,8 @@ public class fromExcel {
                 try {
                     workbook.close(); // Close the workbook to release resources
                 } catch (IOException e) {
-                    e.printStackTrace(); // Log closing error but don't throw
+                    System.out.println("Error closing workbook: " + e.getMessage());
+                     // Log closing error but don't throw
                 }
             }
         }
@@ -68,6 +81,7 @@ public class fromExcel {
             Row row = sheet.getRow(i);
             if (row != null) {
                 Cell cell = row.getCell(0); // Check the first cell for header
+                //cell not blank and cell type not equal to blank and cell value equal to "Latest Start Date"
                 if (cell != null && cell.getStringCellValue().equalsIgnoreCase("Latest Start Date")) {
                     headerRowIndex = i;
                     break;
@@ -98,6 +112,9 @@ public class fromExcel {
                                     count++;
                                 }
                                 break;
+                            case BLANK:
+
+                                break;
                             default:
                                 System.out.println("Unsupported cell type: " + cell.getCellType());
                         }
@@ -113,10 +130,20 @@ public class fromExcel {
 
 
     private static Map<String, String> readRegexFromJson(JsonNode regexRoot) {
+        if (!isJSONValid(regexRoot.toString())) {
+            throw new IllegalArgumentException("Root node must be an array");
+        }
         Map<String, String> datePatternsMap = new HashMap<>();
         for (JsonNode regexNode : regexRoot) {
-            datePatternsMap.put(regexNode.get("regex").asText(), regexNode.get("pattern").asText());
-            System.out.println("Regex loaded: " + regexNode.get("regex").asText() + " | Pattern: " + regexNode.get("pattern").asText());
+            if (regexNode.has("datesFieldName") && regexNode.has("datesFile") && regexNode.has("dateFormat")) {
+                datesField = regexNode.get("datesFieldName").asText();
+                 datesFile = regexNode.get("datesFile").asText();
+                 dateFormat = regexNode.get("dateFormat").asText();
+                System.out.println("Dates Field Name: " + datesField + "\nDates File: " + datesFile + "\nDate Format: " + dateFormat);
+            } else if (regexNode.has("regex") ) {
+                datePatternsMap.put(regexNode.get("pattern").asText(), regexNode.get("regex").asText());
+                System.out.println("Regex loaded: " + regexNode.get("regex").asText() + " | Pattern: " + regexNode.get("pattern").asText());            }
+
         }
         return datePatternsMap;
     }
@@ -126,15 +153,45 @@ public class fromExcel {
 
         for (String input : dateExamples) {
             for (Map.Entry<String, String> entry : datePatternsMap.entrySet()) {
-                Pattern regex = Pattern.compile(entry.getKey());
+                Pattern regex = Pattern.compile(entry.getValue());
                 Matcher matcher = regex.matcher(input);
+
                 if (matcher.matches()) {
-                    results.put(input, entry.getValue());
-                    break;
+                    //change any delimiter like , ; :  / to -
+                    input = input.replaceAll("[,;:\\/_.*]", "-");
+                    String patt = entry.getKey().replaceAll("[,;:\\/_.*]", "-");
+                    validateDatePattern(input, patt);
+                    results.put(input, entry.getKey());
+                    System.out.println("Input: " + input + " | Detected Format: " + entry.getKey());
+
+                }else {
+                    results.put(input, "Unmatched");
                 }
+
             }
         }
 
         return results;
+    }
+    private static void validateDatePattern(String input, String pattern) {
+        Boolean d,m,y = false;
+
+        if (pattern.split("-")[0] == "dd" || pattern.split("-")[0] == "d"){
+            if (Integer.parseInt(input.split("-")[0]) < 31 && Integer.parseInt(input.split("-")[0]) > 12) {
+                d = true;
+            }else {
+                d = false;
+            }
+        } else if (pattern.split("-")[0] == "MM" || pattern.split("-")[0] == "M"){
+            if (Integer.parseInt(input.split("-")[0]) < 12 ) {
+                m = true;
+            } else {
+                m = false;
+            }
+        } else if (pattern.split("-")[0] == "yyyy" || pattern.split("-")[0] == "yy" || pattern.split("-")[0] == "Y"){
+            if (Integer.parseInt(input.split("-")[0]) < 1000) {
+                System.out.println("Invalid date pattern: " + pattern);
+            }
+        }
     }
 }
